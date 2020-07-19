c9d8764c3efed68a2e518cdc8bd7171867aec76f
bizhan71720 <bizhan71720@gamil.com>
2020-07-19 16:22:06 +0430

    add command line program

---
+++ sms.py
@@ -0,0 +1,36 @@
+from ipaddress import IPv4Address
+from pyairmore.request import AirmoreSession
+from pyairmore.services.messaging import MessagingService
+
+def get_data_connection():
+    ip  = input("phone ip:")
+    port = int(input("port(defualt=2333):"))
+    ip4 = IPv4Address(ip)
+    return ip4 , port
+
+def connection():
+    global session
+    if session.is_server_running:
+        print("server is up")
+    if  session.request_authorization() :
+        print("yah, you`r connect")
+    else:
+        connection()
+
+def sending():
+    global session
+    rucontinue = "y"
+    while rucontinue == "y":
+        phone_number = input("insert your taget phone number:")
+        count_massage = int(input("how many times?"))
+        massage = input("your massage:")
+        service = MessagingService(session)
+        for i in range (0,count_massage):
+            print ("massage %s is seding" %str(i+1))
+            service.send_message(phone_number, massage)
+        rucontinue = input("are you want to continue?(y for continue)")
+
+ip4 , port = get_data_connection()
+session = AirmoreSession(ip4 ,port)
+connection()
+sending()
